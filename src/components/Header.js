7ffba0f8ee2833ef3e16979aaa43a0300fbf2f98
Yanni <yannidilly.prog@gmail.com>
2022-10-31 20:41:19 -0300

    estrutura o Header e estiliza o componente para desenvolvimento

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,21 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div>Header</div>
+      <header className="wallet-header">
+        <h1>Gerenciador de Despesas</h1>
+        <section className="info-section">
+          <p
+            className="email-info"
+          >
+            email
+          </p>
+          <p
+            className="cost-info"
+          >
+            custo total
+          </p>
+        </section>
+      </header>
     );
   }
 }
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Header from '../components/Header';
+import '../style/Wallet.css';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div>
+        <Header />
+      </div>
+    );
   }
 }
 
