09104dd1b0c687e89818bb6c9d3f0f0b0e9cff77
Yanni <yannidilly.prog@gmail.com>
2022-10-31 19:19:12 -0300

    implementa estrutura react-html da página

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,60 @@
 import React from 'react';
+import '../style/Login.css';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  onClickButton = () => {
+    const { email, password } = this.state;
+    // jogar o state do componente para o globalState
+    console.log(email);
+    console.log(password);
+  };
+
+  onInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
-    return <div>Login</div>;
+    return (
+      <section
+        className="login-page"
+      >
+        <form
+          className="login-form"
+        >
+          <p>E-mail:</p>
+          <input
+            data-testid="email-input"
+            name="email"
+            onChange={ this.onInputChange }
+          />
+          <p>Senha</p>
+          <input
+            data-testid="password-input"
+            name="password"
+            onChange={ this.onInputChange }
+          />
+          <div>
+            <button
+              type="button"
+              onClick={ this.onClickButton }
+            >
+              Enviar
+            </button>
+          </div>
+        </form>
+      </section>
+    );
   }
 }
 
