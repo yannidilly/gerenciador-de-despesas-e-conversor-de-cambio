a791757f1d2431819ebad9c02c843bac7ee538ed
Yanni <yannidilly.prog@gmail.com>
2022-10-31 19:18:35 -0300

    implementa navegação entre páginas com Route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/carteira" component={ Wallet } />
+      </Switch>
+    );
+  }
 }
 
 export default App;
