a16c8a3d9eacadeed4a2765aba8f9334e4b24507
Yanni <yannidilly.prog@gmail.com>
2022-11-04 12:00:17 -0300

    implementa formatação do retorno da API

--- src/services/getCurrencies.js
+++ src/services/getCurrencies.js
@@ -1,9 +1,17 @@
 const endPoint = 'https://economia.awesomeapi.com.br/json/all';
 
+const formatCurrencies = (currenciesObj) => {
+  const currenciesArray = Object.keys(currenciesObj);
+  const indexUSDT = currenciesArray.indexOf('USDT');
+  currenciesArray.splice(indexUSDT, 1);
+  return currenciesArray;
+};
+
 const getCurrencies = async () => {
   const response = await fetch(endPoint);
   const currencies = await response.json();
-  return currencies;
+  const currenciesFormated = formatCurrencies(currencies);
+  return currenciesFormated;
 };
 
 export default getCurrencies;
