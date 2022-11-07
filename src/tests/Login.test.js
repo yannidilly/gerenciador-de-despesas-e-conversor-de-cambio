417a4b50ddcae5d076acda02aa598427f697a51a
Yanni <yannidilly.prog@gmail.com>
2022-11-07 19:53:07 -0300

    implementa testes que verificam se existem componentes na tela de Login

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,6 +1,16 @@
+import { screen } from '@testing-library/react';
+import { renderWithRouterAndRedux } from './helpers/renderWith';
+import Login from '../pages/Login';
+
 describe('Realiza os testes na página de login', () => {
   test('Testa se existe o input para preencher o email e a senha', () => {
-
+    renderWithRouterAndRedux(<Login />);
+    const emailText = screen.queryByText('E-mail:');
+    expect(emailText).toBeInTheDocument();
+    const emailInput = screen.queryByTestId('email-input');
+    expect(emailInput).toBeInTheDocument();
+    const passwordInput = screen.queryByTestId('password-input');
+    expect(passwordInput).toBeInTheDocument();
   });
   test('Testa se o botão fica desabilitado quando os inputs são preenchidos com informações incorretas', () => {
     // email no formato inválido
