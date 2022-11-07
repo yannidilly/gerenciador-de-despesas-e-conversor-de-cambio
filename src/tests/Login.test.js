da03771e5cad97c9ffd146ffff29d0be07bed3dd
Yanni <yannidilly.prog@gmail.com>
2022-11-06 21:20:54 -0300

    estrutura inicial para testes na página de login

---
+++ src/tests/Login.test.js
@@ -0,0 +1,12 @@
+describe('Realiza os testes na página de login', () => {
+  test('Testa se existe o input para preencher o email e a senha', () => {
+
+  });
+  test('Testa se o botão fica desabilitado quando os inputs são preenchidos com informações incorretas', () => {
+    // email no formato inválido
+    // senha com menos de 6 caracteres
+  });
+  test('Testa se o usuário é enviado para página /carteira ao clicar no botão de login após as informações estarem preenchidas', () => {
+
+  });
+});
