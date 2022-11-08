649478c62b9cb12ec9f1c9eac30a3adb70a965b0
Yanni <yannidilly.prog@gmail.com>
2022-11-08 13:35:53 -0300

    adiciona o cabeçalho da tabela na página Wallet

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import '../style/Table.css';
 
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <table className="expenses-table">
+        <tbody>
+          <tr>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Método de Pagamento</th>
+            <th>Categoria</th>
+            <th>Descrição</th>
+            <th>Editar/Excluir</th>
+          </tr>
+
+        </tbody>
+      </table>
     );
   }
 }
