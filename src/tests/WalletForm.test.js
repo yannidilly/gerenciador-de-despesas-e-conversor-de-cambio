c541bfa510acff16e149538c5db0421b49ff419e
Yanni <yannidilly.prog@gmail.com>
2022-11-07 21:53:01 -0300

    cria estrutura para próximos testes do componente WalletForm

--- src/tests/WalletForm.test.js
+++ src/tests/WalletForm.test.js
@@ -4,6 +4,9 @@ import mockData from './helpers/mockData';
 import WalletForm from '../components/WalletForm';
 
 describe('Realiza testes para o componente WalletForm', () => {
+  test('Testa se todos os campos existem', () => {
+
+  });
   test('Testa se todas as opções de moedas aparecem', () => {
     const initialState = {
       wallet: {
@@ -16,5 +19,8 @@ describe('Realiza testes para o componente WalletForm', () => {
     for (let index = 0; index < currencies.children.length; index += 1) {
       expect(currencies.children[index].textContent).toBe(Object.keys(mockData)[index]);
     }
+  });
+  test('Testa se ao clicar no botão de Adicionar despesa, ela é adicionada no estado global (a despesa e a moeda)', () => {
+
   });
 });
