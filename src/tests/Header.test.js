c3b329c1e963f9a3bdb0d02cd751971815e989c2
Yanni <yannidilly.prog@gmail.com>
2022-11-07 21:18:18 -0300

    implementa teste para ver se email cadastrado aparece no header

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,5 +1,19 @@
-describe('', () => {
-  test('', () => {
+import { screen } from '@testing-library/react';
+import { renderWithRedux } from './helpers/renderWith';
+import Header from '../components/Header';
+
+describe('Realiza os testes do componente Header', () => {
+  test('Testa se existe um campo com o email cadastrado', () => {
+    const initialState = {
+      user: {
+        email: 'teste@teste.com',
+      },
+    };
+    renderWithRedux(<Header />, { initialState });
+    const email = screen.queryByText('teste@teste.com');
+    expect(email).toBeInTheDocument();
+  });
+  test('Testa se existe um campo com as despesas totais', () => {
 
   });
 });
