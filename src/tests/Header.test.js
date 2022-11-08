a28d47cf6f0e5e735e7f6b1456ef2f7b01e17c7a
Yanni <yannidilly.prog@gmail.com>
2022-11-07 21:28:24 -0300

    implementa testes para o campo de despesas totais

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -14,6 +14,14 @@ describe('Realiza os testes do componente Header', () => {
     expect(email).toBeInTheDocument();
   });
   test('Testa se existe um campo com as despesas totais', () => {
-
+    const initialState = {
+      wallet: {
+        totalSpending: 200,
+      },
+    };
+    renderWithRedux(<Header />, { initialState });
+    const expenseQuantity = screen.queryByTestId('total-field');
+    expect(expenseQuantity).toBeInTheDocument();
+    expect(expenseQuantity.textContent).toBe('200.00');
   });
 });
