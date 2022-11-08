4343d73b70b3f9321890e56c65dae79cf0ac1296
Yanni <yannidilly.prog@gmail.com>
2022-11-07 21:51:08 -0300

    implementa testes para verificar as opções de moedas possíveis de serem selecionadas

--- src/tests/WalletForm.test.js
+++ src/tests/WalletForm.test.js
@@ -1,5 +1,20 @@
-describe('', () => {
-  test('', () => {
+import { screen } from '@testing-library/react';
+import { renderWithRedux } from './helpers/renderWith';
+import mockData from './helpers/mockData';
+import WalletForm from '../components/WalletForm';
 
+describe('Realiza testes para o componente WalletForm', () => {
+  test('Testa se todas as opções de moedas aparecem', () => {
+    const initialState = {
+      wallet: {
+        currencies: Object.keys(mockData),
+      },
+    };
+    renderWithRedux(<WalletForm />, { initialState });
+    const currencies = screen.queryByTestId('currency-input');
+    expect(currencies).toBeInTheDocument();
+    for (let index = 0; index < currencies.children.length; index += 1) {
+      expect(currencies.children[index].textContent).toBe(Object.keys(mockData)[index]);
+    }
   });
 });
