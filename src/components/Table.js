10325075ceec0237ae28bb0299dee49e9654be66
Yanni <yannidilly.prog@gmail.com>
2022-11-13 15:09:27 -0300

    modifica estrutura da tabela para se adequar aos testes

--- src/components/Table.js
+++ src/components/Table.js
@@ -9,42 +9,40 @@ class Table extends Component {
     return (
       <table className="expenses-table">
         <tbody>
-          <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
+          <thead>Descrição</thead>
+          <thead>Tag</thead>
+          <thead>Método de pagamento</thead>
+          <thead>Valor</thead>
+          <thead>Moeda</thead>
+          <thead>Câmbio utilizado</thead>
+          <thead>Valor convertido</thead>
+          <thead>Moeda de conversão</thead>
+          <thead>Editar/Excluir</thead>
           {
             expenses.map((expense) => (
               <tr key={ expense.id }>
-                <th>{ expense.description }</th>
-                <th>{ expense.tag }</th>
-                <th>{ expense.method }</th>
-                <th>{ (expense.value * 1).toFixed(2) }</th>
-                <th>
+                <td>{ expense.description }</td>
+                <td>{ expense.tag }</td>
+                <td>{ expense.method }</td>
+                <td>{ (expense.value * 1).toFixed(2) }</td>
+                <td>
                   {
                     expense.exchangeRates[expense.currency].name
                   }
-                </th>
-                <th>
+                </td>
+                <td>
                   {
-                    expense.exchangeRates[expense.currency].ask
+                    (expense.exchangeRates[expense.currency].ask * 1).toFixed(2)
                   }
-                </th>
-                <th>
+                </td>
+                <td>
                   {
                     (expense.value * expense.exchangeRates[expense.currency].ask)
                       .toFixed(2)
                   }
-                </th>
-                <th>Real</th>
-                <th>Editar/Excluir</th>
+                </td>
+                <td>Real</td>
+                <td>Editar/Excluir</td>
               </tr>
             ))
           }
